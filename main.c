d258cb86f4d78616516b14c7d5570f817b30ee90
Henry Jason Biem <jasonbiem@gmail.com>
2015-03-23 15:50:28 -0400

    Version3.

--- main.c
+++ main.c
@@ -6,9 +6,14 @@
 
 int main(int argc, char *argv[])
 {
-        FILE *file_ptr;
+        FILE *file_ptr, *file_ptr2;
         char filename[500];
         char garbage;
+        char beingCareful[500];
+        char lineReading[500];
+        int readNumber;
+        int fileLineLength;
+        int i;
         do {
                 printf("Please provide a valid filepath with the proper extension to be opened: \n");
                 scanf("%499[^\n]", filename);
@@ -22,4 +27,24 @@ int main(int argc, char *argv[])
                 file_ptr=fopen(filename, "rt");
 
         } while (file_ptr ==NULL);
-}
+
+        i=0;
+        strcpy(lineReading, "wc -l < ");
+        strcat(lineReading, filename);
+        strcat(lineReading, " > LineNumber.txt");
+        system(lineReading);
+        file_ptr2= fopen("LineNumber.txt", "rt");
+        fgets(beingCareful, 499, file_ptr2);
+        fileLineLength = atoi(beingCareful);
+        fclose(file_ptr2);
+        printf("%d \n \n", fileLineLength);
+        
+        while(!feof(file_ptr))
+        {    
+        fgets(beingCareful, 499, file_ptr);
+        readNumber= atoi(beingCareful);
+        printf("%d\n", readNumber);
+        i++;
+        if(i==fileLineLength) break; 
+        }
+}   
