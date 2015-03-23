4d7994d3cd6bb495046ba1f1a7d2a7e905ffec42
Henry Jason Biem <jasonbiem@gmail.com>
2015-03-23 15:49:08 -0400

    Version1.2

--- main.c
+++ main.c
@@ -1,1 +1,25 @@
-Main file
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/** Henry Jason Biem 260590556 **/
+
+int main(int argc, char *argv[])
+{
+        FILE *file_ptr;
+        char filename[500];
+        char garbage;
+        do {
+                printf("Please provide a valid filepath with the proper extension to be opened: \n");
+                scanf("%499[^\n]", filename);
+
+                garbage=getchar();
+                while (garbage !=10)
+                {
+                        garbage = getchar();
+                }
+
+                file_ptr=fopen(filename, "rt");
+
+        } while (file_ptr ==NULL);
+}
