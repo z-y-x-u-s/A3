afae4467affc76f0167656503a8558a4e1c47668
Henry Jason Biem <jasonbiem@gmail.com>
2015-03-23 16:05:44 -0400

    Final Version9 with Slight Tweaks. Here was the true timeline on RCS
    First log
    ----------------------------
    revision 1.9
    date: 2015/03/23 18:35:31;  author: hbiem1;  state: Exp;  lines: +27 -27
    Version9!.
    ----------------------------
    revision 1.8
    date: 2015/03/23 17:36:27;  author: hbiem1;  state: Exp;  lines: +43 -43
    Version 8!.
    ----------------------------
    revision 1.7
    date: 2015/03/23 00:25:09;  author: hbiem1;  state: Exp;  lines: +1 -87
    Version7!.
    ----------------------------
    revision 1.6
    date: 2015/03/22 23:33:14;  author: hbiem1;  state: Exp;  lines: +27 -13
    Version6!.
    ----------------------------
    revision 1.5
    date: 2015/03/22 22:54:50;  author: hbiem1;  state: Exp;  lines: +74 -4
    Version5!.
    ----------------------------
    revision 1.4
    date: 2015/03/22 21:10:32;  author: hbiem1;  state: Exp;  lines: +51 -5
    Version4!.
    ----------------------------
    revision 1.3
    date: 2015/03/22 18:46:50;  author: hbiem1;  state: Exp;  lines: +27 -2
    Version3!.
    ----------------------------
    revision 1.2
    date: 2015/03/22 17:00:57;  author: hbiem1;  state: Exp;  lines: +26 -1
    Version2!.
    ----------------------------
    revision 1.1
    date: 2015/03/22 15:22:05;  author: hbiem1;  state: Exp;
    Initial revision
    =============================================================================

--- main.c
+++ main.c
@@ -16,38 +16,38 @@ int main(int argc, char *argv[])
         /*Loops continously until a valid pointer is produced from fopen */
         do {
                 printf("Please provide a valid filepath with the proper extension to be opened: \n");
-                scanf("%499[^\n]", filename);      // Read until the carriage return
+                scanf("%499[^\n]", filename);      // Read until the carriage return.
 
-                garbage=getchar();                 // Remove the carriage return from
+                garbage=getchar();                 // Remove the carriage return from.
 
-                file_ptr=fopen(filename, "rt");    // Open the text file in read mode
+                file_ptr=fopen(filename, "rt");    // Open the text file in read mode.
 
         } while (file_ptr ==NULL);
 
         /* This segment of code counts how many lines are in the inputed file */
-        strcpy(lineReading, "wc -l < ");           // These 3 lines create the string to be systemed out
+        strcpy(lineReading, "wc -l < ");           // These 3 lines create the string to be systemed out.
         strcat(lineReading, filename);
         strcat(lineReading, " > LineNumber.txt");
-        system(lineReading);                       // The line count command is executed in bash
-        file_ptr2= fopen("LineNumber.txt", "rt");  // The resulting file is opened
-        fgets(beingCareful, 499, file_ptr2);       // The first line is retrieved
-        fileLineLength = atoi(beingCareful);       // And then converted to an integer
-        fclose(file_ptr2);                         // The created file is closed
+        system(lineReading);                       // The line count command is executed in bash.
+        file_ptr2= fopen("LineNumber.txt", "rt");  // The resulting file is opened.
+        fgets(beingCareful, 499, file_ptr2);       // The first line is retrieved.
+        fileLineLength = atoi(beingCareful);       // And then converted to an integer.
+        fclose(file_ptr2);                         // The created file is closed.
 
         /*This while loop reads the file to the end */
         while(!feof(file_ptr))
         {
-                fgets(beingCareful, 499, file_ptr);// The current line is read  
-                readNumber= atoi(beingCareful);    // The string is converted to an integer
-                add(readNumber);                   // The integer is added to the node
+                fgets(beingCareful, 499, file_ptr);// The current line is read. 
+                readNumber= atoi(beingCareful);    // The string is converted to an integer.
+                add(readNumber);                   // The integer is added to the node.
 
                 /*This segment of code verifies that no extra integers were added to the linked list
                   This prevents errors with numerous corner case files, which inserted extra numbers*/
-                j++;                               // Increments with each loop
-                if(j==fileLineLength) break;       // Stop if all lines of the file have been read
+                j++;                               // j increments with each loop.
+                if(j==fileLineLength) break;       // Stop if all lines of the file have been read.
         }
-        fclose(file_ptr);                          // The inputed file is closed
-        prettyPrint();                             // Linked list is printed
+        fclose(file_ptr);                          // The inputed file is closed.
+        prettyPrint();                             // Linked list is printed.
 
 
         /* This loop's code  deletes the user's inputed number should it exist in the Linked List
@@ -55,28 +55,28 @@ int main(int argc, char *argv[])
         do
         {
                 printf("What integer would you like to be removed? The first one found will be deleted: \n");
-                scanf("%499[^\n]", beingCareful); // Read until the carriage return
-                readNumber= atoi(beingCareful);   // Convert the string to an integer
-                garbage=getchar();                // Garbage collect the carriage return
+                scanf("%499[^\n]", beingCareful); // Read until the carriage return.
+                readNumber= atoi(beingCareful);   // Convert the string to an integer.
+                garbage=getchar();                // Garbage collect the carriage return.
 
-                if(delete(readNumber))            // If the delete function returns TRUE
+                if(delete(readNumber))            // If the delete function returns TRUE,
                 {                                 // (The number was successfully deleted)
                         printf("NUMBER WAS DELETED! \n");
-                        prettyPrint();            // Display the new Linked List
+                        prettyPrint();            // Display the new Linked List.
                 }
 
-                else                              // If the delete function returns FALSE
+                else                              // If the delete function returns FALSE,
                 {                                 // (The Number could not be found and deleted)
                         printf("NUMBER WAS NOT FOUND! \n");
-                        prettyPrint();            // Display the same Linked List
+                        prettyPrint();            // Display the same Linked List.
                 }
 
                 printf("Would you like to delete another number? Enter either Y, y, Yes, YES, or yes. \n");
-                scanf("%499[^\n]", answer);       // Get the user's input until the carriage return
-                garbage=getchar();                // Garbage collect the carriage return
+                scanf("%499[^\n]", answer);       // Get the user's input until the carriage return.
+                garbage=getchar();                // Garbage collect the carriage return.
 
                 /*If the inputed string matches one of the 5 answers, the boolean variable is set to true (+1) */
-                boolean = 0;                      // The boolean variable is reset to 0 each loop
+                boolean = 0;                      // The boolean variable is reset to 0 each loop.
                 switch(strlen(answer))
                 {
                         case 1:
@@ -91,8 +91,9 @@ int main(int argc, char *argv[])
                         break;
                 }
 
-        } while(boolean ==1);                      // Repeat the loop should the user desire so (boolean became 1) 
+        } while(boolean ==1);                      // Repeat the loop should the user desire so (boolean became 1).
 }
+
 
 
 
