c69b0fe69a76d958df8c40b898dd95a71a78bfdc
Henry Jason Biem <jasonbiem@gmail.com>
2015-03-23 16:01:22 -0400

    Version 8 with Comments!

--- main.c
+++ main.c
@@ -10,73 +10,73 @@
 int main(int argc, char *argv[])
 {
         FILE *file_ptr, *file_ptr2;
-        char filename[500];
-        char garbage;
-        char beingCareful[500], answer[500];
-        char lineReading[500];
-        int readNumber, boolean;
-        int fileLineLength;
-        int j;
+        char garbage, filename[500], beingCareful[500], lineReading[500], answer[500];
+        int readNumber, boolean, fileLineLength, j=0;
+
+        /*Loops continously until a valid pointer is produced from fopen */
         do {
                 printf("Please provide a valid filepath with the proper extension to be opened: \n");
-                scanf("%499[^\n]", filename);
+                scanf("%499[^\n]", filename);      // Read until the carriage return
 
-                garbage=getchar();
-                while (garbage !=10)
-                {
-                        garbage = getchar();
-                }
+                garbage=getchar();                 // Remove the carriage return from
 
-                file_ptr=fopen(filename, "rt");
+                file_ptr=fopen(filename, "rt");    // Open the text file in read mode
 
         } while (file_ptr ==NULL);
 
-        j=0;
-        strcpy(lineReading, "wc -l < ");
+        /* This segment of code counts how many lines are in the inputed file */
+        strcpy(lineReading, "wc -l < ");           // These 3 lines create the string to be systemed out
         strcat(lineReading, filename);
         strcat(lineReading, " > LineNumber.txt");
-        system(lineReading);
-        file_ptr2= fopen("LineNumber.txt", "rt");
-        fgets(beingCareful, 499, file_ptr2);
-        fileLineLength = atoi(beingCareful);
-        fclose(file_ptr2);
+        system(lineReading);                       // The line count command is executed in bash
+        file_ptr2= fopen("LineNumber.txt", "rt");  // The resulting file is opened
+        fgets(beingCareful, 499, file_ptr2);       // The first line is retrieved
+        fileLineLength = atoi(beingCareful);       // And then converted to an integer
+        fclose(file_ptr2);                         // The created file is closed
 
+        /*This while loop reads the file to the end */
         while(!feof(file_ptr))
         {
-        fgets(beingCareful, 499, file_ptr);
-        readNumber= atoi(beingCareful);
-        add(readNumber);
-        j++;
-        if(j==fileLineLength) break;
-        }
-        fclose(file_ptr);
-        prettyPrint();
+                fgets(beingCareful, 499, file_ptr);// The current line is read  
+                readNumber= atoi(beingCareful);    // The string is converted to an integer
+                add(readNumber);                   // The integer is added to the node
 
+                /*This segment of code verifies that no extra integers were added to the linked list
+                  This prevents errors with numerous corner case files, which inserted extra numbers*/
+                j++;                               // Increments with each loop
+                if(j==fileLineLength) break;       // Stop if all lines of the file have been read
+        }
+        fclose(file_ptr);                          // The inputed file is closed
+        prettyPrint();                             // Linked list is printed
+
+
+        /* This loop's code  deletes the user's inputed number should it exist in the Linked List
+           It repeats based on the user's input asking it to do so  */
         do
         {
                 printf("What integer would you like to be removed? The first one found will be deleted: \n");
-                boolean=0;
-                scanf("%499[^\n]", beingCareful);
-                readNumber= atoi(beingCareful);
-                garbage=getchar();
+                scanf("%499[^\n]", beingCareful); // Read until the carriage return
+                readNumber= atoi(beingCareful);   // Convert the string to an integer
+                garbage=getchar();                // Garbage collect the carriage return
 
-                if(delete(readNumber))
-                {
+                if(delete(readNumber))            // If the delete function returns TRUE
+                {                                 // (The number was successfully deleted)
                         printf("NUMBER WAS DELETED! \n");
-                        prettyPrint();
+                        prettyPrint();            // Display the new Linked List
                 }
 
-                else
-                {
+                else                              // If the delete function returns FALSE
+                {                                 // (The Number could not be found and deleted)
                         printf("NUMBER WAS NOT FOUND! \n");
-                        prettyPrint();
+                        prettyPrint();            // Display the same Linked List
                 }
 
                 printf("Would you like to delete another number? Enter either Y, y, Yes, YES, or yes. \n");
-                scanf("%499[^\n]", answer);
-                garbage=getchar();
+                scanf("%499[^\n]", answer);       // Get the user's input until the carriage return
+                garbage=getchar();                // Garbage collect the carriage return
 
                 /*If the inputed string matches one of the 5 answers, the boolean variable is set to true (+1) */
+                boolean = 0;                      // The boolean variable is reset to 0 each loop
                 switch(strlen(answer))
                 {
                         case 1:
@@ -91,7 +91,8 @@ int main(int argc, char *argv[])
                         break;
                 }
 
-        } while(boolean ==1);
+        } while(boolean ==1);                      // Repeat the loop should the user desire so (boolean became 1) 
 }
+
 
 
