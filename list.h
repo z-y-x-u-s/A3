356b6be8194a59772c34ff6b33159845de6c35ee
Henry Jason Biem <jasonbiem@gmail.com>
2015-03-23 16:16:06 -0400

    Header File

---
+++ list.h
@@ -0,0 +1,10 @@
+/* Henry Jason Biem 260590556 */
+
+void add(int value);
+void prettyPrint();
+typedef int BOOLEAN;
+BOOLEAN delete(int input);
+
+/*Above are the function signatures and typedef
+necessary for main.c to use list.c's functions */
+
